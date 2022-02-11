0ca17efcebe88e01be9d3c2386ed46707248b8c2
NickGontarenko <nick.gontarenko@gmail.com>
2022-02-11 21:12:20 +0200

    task-01-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const startBtnRef = document.querySelector('[data-start]');
+// console.log(startBtnRef);
+const stopBtnRef = document.querySelector('[data-stop]');
+// console.log(stopBtnRef);
+let isActiv = false;
+let timerId = null;
 
+startBtnRef.addEventListener('click', onStartChangeBodyBgc);
+stopBtnRef.addEventListener('click', onStopChangeBodyBgc);
+stopBtnRef.setAttribute('disabled', true);
+
+function onStartChangeBodyBgc() {
+  if (isActiv) {
+    return;
+  }
+
+  startBtnRef.setAttribute('disabled', true);
+  stopBtnRef.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    console.log(randomColor);
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+  isActiv = true;
+}
+
+function onStopChangeBodyBgc() {
+  clearInterval(timerId);
+  isActiv = false;
+  startBtnRef.removeAttribute('disabled');
+  stopBtnRef.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
